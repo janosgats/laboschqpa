9bd263ee8f53f0f51407e3aa4e74c762ca46bb91
Dzsani <gatsjanos@gmail.com>
2020-03-25 15:25:53 +0100

    Refactor from 'cst' to 'qpa'.

--- clone-downstream-repos.ps1
+++ clone-downstream-repos.ps1
@@ -1,11 +1,11 @@
 #server code
-git clone https://github.com/janosgats/laboschcst.server.git
+git clone https://github.com/janosgats/laboschqpa.server.git
 
 #filehost code
-git clone https://github.com/janosgats/laboschcst.filehost.git
+git clone https://github.com/janosgats/laboschqpa.filehost.git
 
 #frontend code 
-git clone https://github.com/janosgats/laboschcst.frontend.git
+git clone https://github.com/janosgats/laboschqpa.frontend.git
 
 #k8s config files
-git clone https://github.com/janosgats/laboschcst.k8s.git
+git clone https://github.com/janosgats/laboschqpa.k8s.git
--- start-dev-env.ps1
+++ start-dev-env.ps1
@@ -1,16 +1,16 @@
 #secrets
-kubectl delete secret laboschcst-server-secrets
-kubectl delete secret laboschcst-filehost-secrets
-kubectl create secret generic laboschcst-server-secrets --from-file=secrets.properties=laboschcst.k8s/setting_up_dev_env/server/secret/secrets-k8s_dev.properties
-kubectl create secret generic laboschcst-filehost-secrets --from-file=secrets.properties=laboschcst.k8s/setting_up_dev_env/filehost/secret/secrets-k8s_dev.properties
+kubectl delete secret laboschqpa-server-secrets
+kubectl delete secret laboschqpa-filehost-secrets
+kubectl create secret generic laboschqpa-server-secrets --from-file=secrets.properties=laboschqpa.k8s/setting_up_dev_env/server/secret/secrets-k8s_dev.properties
+kubectl create secret generic laboschqpa-filehost-secrets --from-file=secrets.properties=laboschqpa.k8s/setting_up_dev_env/filehost/secret/secrets-k8s_dev.properties
 
 #dev cluster resources
-kubectl apply -f laboschcst.k8s/setting_up_dev_env
+kubectl apply -f laboschqpa.k8s/setting_up_dev_env
 
 #forcing Nginx to set up before Skaffold applies config of the particular microservices
-kubectl apply -f laboschcst.k8s/dev/ingress-nginx
-kubectl apply -f laboschcst.k8s/dev/ingress-nginx
-kubectl apply -f laboschcst.k8s/dev/ingress-nginx
+kubectl apply -f laboschqpa.k8s/dev/ingress-nginx
+kubectl apply -f laboschqpa.k8s/dev/ingress-nginx
+kubectl apply -f laboschqpa.k8s/dev/ingress-nginx
 
 #starting Skaffold in "dev" mode
 skaffold dev
