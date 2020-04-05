7ee9e672afc8e98f1537bc2af42f15099e345955
Dzsani <gatsjanos@gmail.com>
2020-04-05 04:08:53 +0200

    Dev env resources reorganized. Modified startup script.

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ The contained repositories are:
 1. Clone this repo
 2. Run `clone-downstream-repos.ps1` to clone the above repositories
 3. Place development secrets under `laboschqpa.k8s/setting_up_dev_env/<service_name>/secret/`
-4. Edit *.yaml* files in `laboschqpa.k8s/setting_up_dev_env/` to provide PC specific resources for your local k8s cluster (currently, this means setting the `size` and `hostPath` of `pv-volume-filehost-storedfiles.yaml`)
+4. Edit *.yaml* files in `laboschqpa.k8s/setting_up_dev_env/` to provide PC specific resources for your local k8s cluster (this mostly means setting the `capacity->storage` and `hostPath` of `pv/pv-volume-nfs-server.yaml`)
 5. Build the **Java**-based services by `mvn package` and the **JS** services by `yarn install` (or `npm install`) for the first time on your PC
 6. Have a (local) k8s cluster running and *kubectl* configured to use that cluster
 7. Have [Skaffold](https://skaffold.dev/) installed
--- start-dev-env.ps1
+++ start-dev-env.ps1
@@ -1,8 +1,21 @@
+function EchoHeadline($title)
+{
+    $headLine = "==================================================================== " + $title + " ===================================================================="
+    echo $headLine
+}
+
+function EchoSmallHeadline($title)
+{
+    $headLine = "-------------------------------------------------------- " + $title
+    echo $headLine
+}
+
+
+#Repeating 'apply'-s several times, in case some resources depend on other resources (e.g. namespaces created).
 function Apply-Config-Surely($title, $configCommand)
 {
-	$headLine = "==================================================================== " + $title + " ===================================================================="
-	echo $headLine
-	
+    EchoHeadline $title
+
 	For ($i=1; $i -le 3; $i++) {
 		$msg = "------------------------------(" + $i + ")--------------------------------------------(" + $i + ")"
 		echo $msg
@@ -10,22 +23,59 @@ function Apply-Config-Surely($title, $configCommand)
     }
 }
 
+function Get-PSScriptRoot-Path-For-Kubernetes()
+{
+    $driveLetter = $PSScriptRoot.Substring(0,1).ToLower()
+    $kubernetesPath = "/host_mnt/" + $driveLetter + "/" + $PSScriptRoot.Substring(3)
+    return $kubernetesPath
+}
+
+function Create-PV-For-Node-Modules()
+{
+    EchoHeadline "Creating PV for node_modules"
+    EchoSmallHeadline "Determining path of node_modules directory"
+
+    $nodeModulesPath = Get-PSScriptRoot-Path-For-Kubernetes
+    $nodeModulesPath = $nodeModulesPath + "/laboschqpa.frontend/node_modules"
+
+    $msg = "nodeModulesPath: " + $nodeModulesPath
+    echo $msg
+
+    $pvOriginalYaml = Get-Content -path laboschqpa.k8s/setting_up_dev_env/pv/pv-volume-frontend-nodemodules-for-dev.yaml -Raw
+    $pvPathInsertedYaml = $pvOriginalYaml -replace 'DEV_ENV_FRONTEND_NODE_MODULES_FOLDER_HOST_PATH_PLACEHOLDER', $nodeModulesPath
+
+    #EchoSmallHeadline "Deleting old nodemodules PV"
+    #kubectl delete pv pv-volume-frontend-nodemodules-for-dev
+    EchoSmallHeadline "Applying new nodemodules PV"
+    echo $pvPathInsertedYaml | kubectl apply -f -
+}
+
 #secrets
-echo "==================================================================== Managing Secrets ===================================================================="
+EchoHeadline "Managing Secrets"
 kubectl delete secret laboschqpa-server-secrets
 kubectl delete secret laboschqpa-filehost-secrets
 kubectl create secret generic laboschqpa-server-secrets --from-file=secrets.properties=laboschqpa.k8s/setting_up_dev_env/server/secret/secrets-k8s_dev.properties
 kubectl create secret generic laboschqpa-filehost-secrets --from-file=secrets.properties=laboschqpa.k8s/setting_up_dev_env/filehost/secret/secrets-k8s_dev.properties
 
-
-#dev cluster resources
-Apply-Config-Surely "Applying dev cluster specific config" "kubectl apply -f ${PWD}/laboschqpa.k8s/setting_up_dev_env"
+Apply-Config-Surely "Applying miscellaneous cluster specific config" "kubectl apply -f ${PWD}/laboschqpa.k8s/setting_up_dev_env"
 
 #forcing Nginx to set up before Skaffold applies config of the particular microservices
 Apply-Config-Surely "Applying Nginx config" "kubectl apply -f laboschqpa.k8s/dev/ingress-nginx"
 
-#applying database config now, so it will be available when the microservices start up
-Apply-Config-Surely "Applying DB config" "kubectl apply -f laboschqpa.k8s/dev/db"
+
+Create-PV-For-Node-Modules
+
+EchoHeadline "Applying Persistent Volumes config"
+kubectl apply -f laboschqpa.k8s/setting_up_dev_env/pv
+
+EchoHeadline "Applying NFS Server config"
+kubectl apply -f laboschqpa.k8s/setting_up_dev_env/nfs-server
+
+EchoHeadline "Applying DB config"
+kubectl apply -f laboschqpa.k8s/setting_up_dev_env/db
+
+EchoHeadline "Applying dev resources (dev_res)"
+kubectl apply -f laboschqpa.k8s/setting_up_dev_env/dev_res
 
 
 #print the bearer token required for dashboard login
@@ -35,6 +85,6 @@ kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboar
 echo "==========================================================================================================================================================="
 
 
-#starting Skaffold in "dev" mode
-echo "================================================================= Starting Skaffold 'dev' ================================================================="
-Start-Process "skaffold" "dev" -NoNewWindow -Wait -PassThru
+#starting Skaffold
+EchoHeadline "Starting Skaffold 'dev'"
+Start-Process "skaffold" "dev --cleanup=false" -NoNewWindow -Wait -PassThru
