a6269a614218d097ec92ae83aec3b57e8912e065
Dzsani <gatsjanos@gmail.com>
2020-06-01 05:17:19 +0200

    Running 'skaffold delete' explicitly at the end of start-dev-env.ps1

--- start-dev-env.ps1
+++ start-dev-env.ps1
@@ -1,24 +1,24 @@
-function EchoHeadline($title)
+function WriteHeadline($title)
 {
     $headLine = "==================================================================== " + $title + " ===================================================================="
-    echo $headLine
+    Write-Host $headLine
 }
 
-function EchoSmallHeadline($title)
+function WriteSmallHeadline($title)
 {
     $headLine = "-------------------------------------------------------- " + $title
-    echo $headLine
+    Write-Host $headLine
 }
 
 
 #Repeating 'apply'-s several times, in case some resources depend on other resources (e.g. namespaces created).
 function Apply-Config-Surely($title, $configCommand)
 {
-    EchoHeadline $title
+    WriteHeadline $title
 
 	For ($i=1; $i -le 3; $i++) {
 		$msg = "------------------------------(" + $i + ")--------------------------------------------(" + $i + ")"
-		echo $msg
+		Write-Host $msg
 		Invoke-Expression $configCommand
     }
 }
@@ -32,29 +32,29 @@ function Get-PSScriptRoot-Path-For-Kubernetes()
 
 function Create-PV-For-Node-Modules()
 {
-    EchoHeadline "Creating PV for node_modules"
-    EchoSmallHeadline "Determining path of node_modules directory"
+    WriteHeadline "Creating PV for node_modules"
+    WriteSmallHeadline "Determining path of node_modules directory"
 
     $nodeModulesPath = Get-PSScriptRoot-Path-For-Kubernetes
     $nodeModulesPath = $nodeModulesPath + "/laboschqpa.frontend/node_modules"
 
     $msg = "nodeModulesPath: " + $nodeModulesPath
-    echo $msg
+    Write-Host $msg
 
     $pvOriginalYaml = Get-Content -path laboschqpa.k8s/setting_up_dev_env/pv/pv-volume-frontend-nodemodules-for-dev.yaml -Raw
     $pvPathInsertedYaml = $pvOriginalYaml -replace 'DEV_ENV_FRONTEND_NODE_MODULES_FOLDER_HOST_PATH_PLACEHOLDER', $nodeModulesPath
 
     #EchoSmallHeadline "Deleting old nodemodules PV"
     #kubectl delete pv pv-volume-frontend-nodemodules-for-dev
-    EchoSmallHeadline "Applying new nodemodules PV"
+    WriteSmallHeadline "Applying new nodemodules PV"
     echo $pvPathInsertedYaml | kubectl apply -f -
 }
 
-EchoHeadline "Loading nfsd kernel module (modprobe nfsd)"
+WriteHeadline "Loading nfsd kernel module (modprobe nfsd)"
 kubectl exec -it -n kube-system $(kubectl get pods -n kube-system | grep kube-proxy | awk '{ print $1 }') modprobe nfsd
 
 #secrets
-EchoHeadline "Managing Secrets"
+WriteHeadline "Managing Secrets"
 kubectl delete secret laboschqpa-server-secrets
 kubectl delete secret laboschqpa-filehost-secrets
 kubectl create secret generic laboschqpa-server-secrets --from-file=secrets.properties=laboschqpa.k8s/setting_up_dev_env/server/secret/secrets-k8s_dev.properties
@@ -70,34 +70,41 @@ Apply-Config-Surely "Applying Metrics config" "kubectl apply -f laboschqpa.k8s/d
 
 #Create-PV-For-Node-Modules
 
-EchoHeadline "Applying Persistent Volumes config"
+WriteHeadline "Applying Persistent Volumes config"
 kubectl apply -f laboschqpa.k8s/setting_up_dev_env/pv
 
-EchoHeadline "Applying NFS Server config"
+WriteHeadline "Applying NFS Server config"
 kubectl apply -f laboschqpa.k8s/setting_up_dev_env/nfs-server
 
-EchoHeadline "Applying DB config"
+WriteHeadline "Applying DB config"
 kubectl apply -f laboschqpa.k8s/setting_up_dev_env/db
 
-EchoHeadline "Applying dev resources (dev_res)"
+WriteHeadline "Applying dev resources (dev_res)"
 kubectl apply -f laboschqpa.k8s/setting_up_dev_env/dev_res
 
-EchoHeadline "Applying Persistent Volumes Claims"
+WriteHeadline "Applying Persistent Volumes Claims"
 kubectl apply -f laboschqpa.k8s/dev/pvc
 
-EchoHeadline "Pulling master images from DockerHub"
+WriteHeadline "Pulling master images from DockerHub"
 docker pull gjani/laboschqpa-server:master
 docker pull gjani/laboschqpa-filehost:master
 docker pull gjani/laboschqpa-frontentd:master
 
 
 #print the bearer token required for dashboard login
-echo "==========================================================================================================================================================="
-echo "===================================================================== DASHBOARD TOKEN ====================================================================="
+Write-Host "==========================================================================================================================================================="
+Write-Host "===================================================================== DASHBOARD TOKEN ====================================================================="
 kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | sls admin-user | ForEach-Object { $_ -Split '\s+' } | Select -First 1)
-echo "==========================================================================================================================================================="
+Write-Host "==========================================================================================================================================================="
 
-
-#starting Skaffold
-EchoHeadline "Starting Skaffold 'dev'"
-Start-Process "skaffold" "dev" -NoNewWindow -Wait -PassThru
+try
+{
+    ##starting Skaffold
+    WriteHeadline "Starting Skaffold 'dev'"
+    Start-Process "skaffold" "dev" -NoNewWindow -Wait -PassThru
+}
+finally
+{
+    WriteHeadline "Running 'skaffold delete' explicitly"
+    Start-Process "skaffold" "delete" -NoNewWindow -Wait -PassThru
+}
